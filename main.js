fb0d77e97524b125dbad4fae0e95fb8c03198c17
Adrian Chudy <adrianchudy_87@o2.pl>
2018-10-10 10:38:15 +0200

    add main.js with clean function

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ const init = () => {
     document.body.appendChild(btn);
     btn.textContent = "Dodaj 10 elementów listy";
     btn.style.fontSize = "30px";
+    const btnClean = document.createElement("button");
+    document.body.appendChild(btnClean);
+    btnClean.textContent = "Wyczyść";
+    btnClean.style.fontSize = "30px";
+    btnClean.style.marginLeft = "10px";
     const ul = document.createElement("ul");
     document.body.appendChild(ul);
-    btn.addEventListener("click", (createLiElements))
+    btn.addEventListener("click", (createLiElements));
+    btnClean.addEventListener("click", cleanList);
 }
 
 const createLiElements = () => {
@@ -20,6 +26,12 @@ const createLiElements = () => {
         liItems.style.fontSize = `${size++}px`;
     }
 
+}
+
+const cleanList = () => {
+    document.querySelector("ul").textContent = "";
+    size = 10;
+    orderElement = 1;
 }
 
 init()
